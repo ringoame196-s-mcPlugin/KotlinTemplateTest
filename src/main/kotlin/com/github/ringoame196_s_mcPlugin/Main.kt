5845e4e4fc2f881b8b611d0a81822be3f6671b27
りんご飴196 <kazumac41@gmail.com>
2024-09-23 16:58:09 +0900

    チェックコード

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -7,7 +7,7 @@ class Main : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
         val plugin = this
-        server.pluginManager.registerEvents(Events(), plugin ) // ココ修正テスト2
+        server.pluginManager.registerEvents(Events(), plugin) // ココ修正テスト2
         // val command = getCommand("command")
         // command!!.setExecutor(Command())
         // command.tabCompleter = TabCompleter()
